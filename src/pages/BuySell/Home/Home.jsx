9912623622ba7baf8ed3af75d4ff54887e852c38
NimishToshniwal <85169722+NimishToshniwal@users.noreply.github.com>
2024-01-08 18:47:45 +0530

    products page spinner

--- src/pages/BuySell/Home/Home.jsx
+++ src/pages/BuySell/Home/Home.jsx
@@ -4,6 +4,7 @@ import Products from '../Products/Products'
 import { Box, Drawer, IconButton, Stack, useMediaQuery } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 import customFetch from '../../../utils/axios.js'
+import Spinner from '../../Auth/Warning/Spinner.js';
 // import axios from 'axios'
 
 const Home = () => {
@@ -39,45 +40,50 @@ const Home = () => {
   };
 
   return (
-    <Box>
-      <Stack direction="row" justifyContent={'space-betweeen'}>
-        {isSmallScreen && (<IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={toggleDrawer}
-          sx={{ display: { md: 'none' } }}
-        >
-          <MenuIcon
-            style={{
-              position: "fixed",
-              top: 70,
-              left: 15,
-              zIndex: 1000,
-              color:'white',
-              backgroundColor:'black',
-              borderRadius:'25%',
-            }} />
-        </IconButton>)}
-        {isSmallScreen ? (
-          <Drawer
-            anchor="left"
-            open={isDrawerOpen}
-            onClose={toggleDrawer}
-            sx={{ display: { xs: 'block', md: 'none' } }}
-          >
-            <Filter categories={categories} setCategories={setCategories} sortBy={sortBy} setSortBy={setSortBy} />
-          </Drawer>
-        ) : (
-          <Box sx={{ position: 'sticky', top: '0', flex: '1', minWidth: 'calc(270px)' }}>
-            <Filter categories={categories} setCategories={setCategories} sortBy={sortBy} setSortBy={setSortBy} />
-          </Box>
-        )}
-        <Box sx={{ maxHeight: 'calc(100vh - 70px)', overflowY: 'auto', flex: '5' }}>
-          <Products productsData={productsData} />
-        </Box>
-      </Stack>
-    </Box>
+    <>
+      {productsData ?
+        <Box>
+          <Stack direction="row" justifyContent={'space-betweeen'}>
+            {isSmallScreen && (<IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={toggleDrawer}
+              sx={{ display: { md: 'none' } }}
+            >
+              <MenuIcon
+                style={{
+                  position: "fixed",
+                  top: 70,
+                  left: 15,
+                  zIndex: 1000,
+                  color: 'white',
+                  backgroundColor: 'black',
+                  borderRadius: '25%',
+                }} />
+            </IconButton>)}
+            {isSmallScreen ? (
+              <Drawer
+                anchor="left"
+                open={isDrawerOpen}
+                onClose={toggleDrawer}
+                sx={{ display: { xs: 'block', md: 'none' } }}
+              >
+                <Filter categories={categories} setCategories={setCategories} sortBy={sortBy} setSortBy={setSortBy} />
+              </Drawer>
+            ) : (
+              <Box sx={{ position: 'sticky', top: '0', flex: '1', minWidth: 'calc(270px)' }}>
+                <Filter categories={categories} setCategories={setCategories} sortBy={sortBy} setSortBy={setSortBy} />
+              </Box>
+            )}
+            <Box sx={{ maxHeight: 'calc(100vh - 70px)', overflowY: 'auto', flex: '5' }}>
+              <Products productsData={productsData} />
+            </Box>
+          </Stack>
+        </Box> : <Spinner />}
+
+    </>
+
   )
 }
 
